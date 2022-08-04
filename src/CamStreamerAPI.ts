a1b55877e4caf5d09be207e4d8ab776ef2a46dfe
Vaclav Stupka <vaclav.stupka@netrex.cz>
2022-08-04 14:02:34 +0200

    Moved CamStreamerAPI.ts into CamStreamerAPI.js

---
+++ src/CamStreamerAPI.ts
@@ -0,0 +1,59 @@
+import {httpRequest} from './HTTPRequest';
+import {HttpRequestOptions} from './HTTPRequest';
+
+export type CamStreamerAPIOptions = {
+    protocol: string;
+    ip: string;
+    port: number;
+    auth: string;
+}
+
+export class CamStreamerAPI {
+    private protocol: string;
+    private ip: string;
+    private port: number;
+    private auth: string;
+
+    constructor(options?: CamStreamerAPIOptions) 
+    {
+        this.protocol = options?.protocol ?? 'http';
+        this.ip = options?.ip ?? '127.0.0.1';
+        this.port = options?.port ?? (this.protocol == 'http') ? 80 : 443;
+        this.auth = options?.auth ?? '';
+    }
+
+    async getStreamList() {
+        const streamListRes = await this.get('/local/camstreamer/stream/list.cgi')
+        return streamListRes.data;
+    }
+
+    async getStreamParameter(streamID: string, paramName: string) {
+        const stream = await this.get(`/local/camstreamer/stream/get.cgi?stream_id=${streamID}`)
+        return stream.data[paramName];
+    }
+
+    async setStreamParameter(streamID: string, paramName: string, value: string) {
+        return await this.get(`/local/camstreamer/stream/set.cgi?stream_id=${streamID}&${paramName}=${value}`);
+    }
+
+    async isStreaming(streamID: string) {
+        const response = await this.get(`/local/camstreamer/get_streamstat.cgi?stream_id=${streamID}`);
+        return response.data.is_streaming;
+    }
+
+    async get(path: string) {
+        const options = this.getBaseConnectionParams();
+        options.path = encodeURI(path);
+        const data = await httpRequest(options) as string;
+        return JSON.parse(data);
+    }
+
+    private getBaseConnectionParams(): HttpRequestOptions {
+        return {
+            protocol: this.protocol + ':',
+            host: this.ip,
+            port: this.port,
+            auth: this.auth
+        };
+    }
+}
