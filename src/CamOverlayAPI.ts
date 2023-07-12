933450b0cc437c1598d9b7904967fea2d700e806
Václav Stupka <vaclav.stupka@netrex.cz>
2023-07-12 09:40:33 +0200

    Removed functions for drawing from CamOverlayAPI.

--- src/CamOverlayAPI.ts
+++ src/CamOverlayAPI.ts
@@ -1,19 +1,13 @@
-import * as WebSocket from 'ws';
-import * as EventEmitter from 'events';
-
-import { Digest } from './Digest';
 import { httpRequest, HttpRequestOptions } from './HTTPRequest';
 
+import * as EventEmitter from 'events';
+
 export type CamOverlayOptions = {
-    protocol?: string; // deprecated (replaced by tls)
     tls?: boolean;
     tlsInsecure?: boolean; // Ignore HTTPS certificate validation (insecure)
     ip?: string;
     port?: number;
     auth?: string;
-    serviceName?: string;
-    serviceID?: number;
-    camera?: number | number[];
 };
 
 export type Field = {
@@ -21,32 +15,6 @@ export type Field = {
     text: string;
     color?: string;
 };
-
-export type Message = {
-    command: string;
-    params: any[];
-};
-
-export type CairoResponse = {
-    message: string;
-    call_id: number;
-};
-
-export type CairoCreateResponse = {
-    var: string;
-    call_id: number;
-};
-
-export type UploadImageResponse = {
-    var: string;
-    width: number;
-    height: number;
-    call_id: number;
-};
-
-export type Align = 'A_RIGHT' | 'A_LEFT' | 'A_CENTER';
-export type TextFit = 'TFM_SCALE' | 'TFM_TRUNCATE' | 'TFM_OVERFLOW';
-export type WriteTextParams = [string, string, number, number, number, number, Align, TextFit?];
 
 export type Service = {
     id: number;
@@ -66,115 +34,23 @@ export enum ImageType {
     JPEG,
 }
 
-type AsyncMessage = {
-    resolve;
-    reject;
-};
-
 export class CamOverlayAPI extends EventEmitter {
     private tls: boolean;
     private tlsInsecure: boolean;
     private ip: string;
     private port: number;
     private auth: string;
-    private serviceName: string;
     private serviceID: number;
-    private cameraList: number[];
-    private callId: number;
-    private sendMessages: Record<number, AsyncMessage>;
-
-    private ws: WebSocket = null;
 
     constructor(options?: CamOverlayOptions) {
         super();
 
         this.tls = options?.tls ?? false;
-        if (options?.tls === undefined && options?.protocol !== undefined) {
-            this.tls = options.protocol === 'wss';
-        }
         this.tlsInsecure = options?.tlsInsecure ?? false;
         this.ip = options?.ip ?? '127.0.0.1';
         this.port = options?.port ?? (this.tls ? 443 : 80);
-        this.auth = options?.auth ?? '';
-        this.serviceName = options?.serviceName ?? '';
-        this.serviceID = options?.serviceID ?? -1; // If service is already created you can skip creation step by filling this parameter
-
-        this.cameraList = [0];
-        if (Array.isArray(options?.camera)) {
-            this.cameraList = options.camera;
-        } else if (typeof options?.camera === 'number') {
-            this.cameraList = [options.camera];
-        }
-
-        this.callId = 0;
-        this.sendMessages = {};
-
+        
         EventEmitter.call(this);
-    }
-
-    async connect() {
-        try {
-            if (this.serviceID === -1) {
-                this.serviceID = await this.createService();
-            }
-            await this.openWebsocket();
-            this.emit('open');
-        } catch (err) {
-            this.reportErr(err);
-        }
-    }
-
-    async createService() {
-        const options = this.getBaseVapixConnectionParams();
-        options.method = 'GET';
-        options.path = '/local/camoverlay/api/services.cgi?action=get';
-        const response = (await httpRequest(options)) as string;
-        let servicesJson: ServiceList;
-        try {
-            servicesJson = JSON.parse(response);
-            servicesJson.services ??= [];
-        } catch {
-            servicesJson = { services: [] };
-        }
-
-        // Find service
-        let service: Service = null;
-        let maxID = -1;
-        let servicesArr = servicesJson.services;
-        for (let s of servicesArr) {
-            if (s.id > maxID) {
-                maxID = s.id;
-            }
-            if (s.identifier === this.serviceName && s.name === 'scripter') {
-                service = s;
-                break;
-            }
-        }
-
-        if (service !== null) {
-            // Check and update service parameters if necessary
-            if (service.cameraList === undefined || !this.compareCameraList(service.cameraList)) {
-                service.cameraList = this.cameraList;
-                await this.updateServices(servicesJson);
-                return service.id as number;
-            } else {
-                return service.id as number;
-            }
-        } else {
-            // Create new service
-            let newServiceID = maxID + 1;
-            service = {
-                id: newServiceID,
-                enabled: 1,
-                schedule: '',
-                name: 'scripter',
-                identifier: this.serviceName,
-                cameraList: this.cameraList,
-            };
-            servicesJson.services.push(service);
-            await this.updateServices(servicesJson);
-            return newServiceID;
-        }
     }
 
     async updateServices(servicesJson: ServiceList) {
@@ -182,183 +58,6 @@ export class CamOverlayAPI extends EventEmitter {
         options.method = 'POST';
         options.path = '/local/camoverlay/api/services.cgi?action=set';
         await httpRequest(options, JSON.stringify(servicesJson));
-    }
-
-    openWebsocket(digestHeader?: string) {
-        let promise = new Promise<void>((resolve, reject) => {
-            const userPass = this.auth.split(':');
-            const protocol = this.tls ? 'wss' : 'ws';
-            const addr = `${protocol}://${this.ip}:${this.port}/local/camoverlay/ws`;
-
-            const options = {
-                auth: this.auth,
-                rejectUnauthorized: !this.tlsInsecure,
-                headers: {},
-            };
-            if (digestHeader !== undefined) {
-                options.headers['Authorization'] = Digest.getAuthHeader(
-                    userPass[0],
-                    userPass[1],
-                    'GET',
-                    '/local/camoverlay/ws',
-                    digestHeader
-                );
-            }
-
-            this.ws = new WebSocket(addr, 'cairo-api', options);
-            this.ws.binaryType = 'arraybuffer';
-
-            this.ws.isAlive = true;
-            const pingTimer = setInterval(() => {
-                if (this.ws.readyState !== this.ws.OPEN || this.ws.isAlive === false) {
-                    return this.ws.terminate();
-                }
-                this.ws.isAlive = false;
-                this.ws.ping();
-            }, 30000);
-
-            this.ws.on('open', () => {
-                this.reportMsg('Websocket opened');
-                resolve();
-            });
-
-            this.ws.on('pong', () => {
-                this.ws.isAlive = true;
-            });
-
-            this.ws.on('message', (data: string) => {
-                let dataJSON = JSON.parse(data);
-                if (dataJSON.hasOwnProperty('call_id') && dataJSON['call_id'] in this.sendMessages) {
-                    if (dataJSON.hasOwnProperty('error')) {
-                        this.sendMessages[dataJSON['call_id']].reject(new Error(dataJSON.error));
-                    } else {
-                        this.sendMessages[dataJSON['call_id']].resolve(dataJSON);
-                    }
-                    delete this.sendMessages[dataJSON['call_id']];
-                }
-
-                if (dataJSON.hasOwnProperty('error')) {
-                    this.reportErr(new Error(dataJSON.error));
-                } else {
-                    this.reportMsg(data);
-                }
-            });
-
-            this.ws.on('unexpected-response', async (req, res) => {
-                if (res.statusCode === 401 && res.headers['www-authenticate'] !== undefined)
-                    this.openWebsocket(res.headers['www-authenticate']).then(resolve, reject);
-                else {
-                    reject('Error: status code: ' + res.statusCode + ', ' + res.data);
-                }
-            });
-
-            this.ws.on('error', (error: Error) => {
-                this.reportErr(error);
-                reject(error);
-            });
-
-            this.ws.on('close', () => {
-                clearInterval(pingTimer);
-                this.reportClose();
-            });
-        });
-        return promise;
-    }
-
-    cairo(command: string, ...params: any[]) {
-        return this.sendMessage({ command: command, params: params }) as Promise<CairoResponse | CairoCreateResponse>;
-    }
-
-    writeText(...params: WriteTextParams) {
-        return this.sendMessage({ command: 'write_text', params: params }) as Promise<CairoResponse>;
-    }
-
-    uploadImageData(imgBuffer: Buffer) {
-        return this.sendBinaryMessage(
-            {
-                command: 'upload_image_data',
-                params: [],
-            },
-            imgBuffer
-        ) as Promise<UploadImageResponse>;
-    }
-
-    uploadFontData(fontBuffer: Buffer) {
-        return this.sendBinaryMessage(
-            {
-                command: 'upload_font_data',
-                params: [fontBuffer.toString('base64')],
-            },
-            fontBuffer
-        ) as Promise<CairoCreateResponse>;
-    }
-
-    showCairoImage(cairoImage: string, posX: number, posY: number) {
-        return this.sendMessage({
-            command: 'show_cairo_image',
-            params: [this.serviceID, cairoImage, posX, posY],
-        }) as Promise<CairoResponse>;
-    }
-
-    showCairoImageAbsolute(cairoImage: string, posX: number, posY: number, width: number, height: number) {
-        return this.sendMessage({
-            command: 'show_cairo_image',
-            params: [this.serviceID, cairoImage, -1.0 + (2.0 / width) * posX, -1.0 + (2.0 / height) * posY],
-        }) as Promise<CairoResponse>;
-    }
-
-    removeImage() {
-        return this.sendMessage({ command: 'remove_image', params: [this.serviceID] }) as Promise<CairoResponse>;
-    }
-
-    private sendMessage(msgJson: Message) {
-        return new Promise<CairoResponse | CairoCreateResponse | UploadImageResponse>((resolve, reject) => {
-            try {
-                this.sendMessages[this.callId] = { resolve, reject };
-                msgJson['call_id'] = this.callId++;
-                this.ws.send(JSON.stringify(msgJson));
-            } catch (err) {
-                this.reportErr(new Error(`Send message error: ${err}`));
-            }
-        });
-    }
-
-    private sendBinaryMessage(msgJson: Message, data: Buffer) {
-        return new Promise<CairoResponse | CairoCreateResponse | UploadImageResponse>((resolve, reject) => {
-            try {
-                this.sendMessages[this.callId] = { resolve, reject };
-                msgJson['call_id'] = this.callId++;
-
-                const jsonBuffer = Buffer.from(JSON.stringify(msgJson));
-
-                const header = new ArrayBuffer(5);
-                const headerView = new DataView(header);
-                headerView.setInt8(0, 1);
-                headerView.setInt32(1, jsonBuffer.byteLength);
-
-                const msgBuffer = Buffer.concat([Buffer.from(header), jsonBuffer, data]);
-                this.ws.send(msgBuffer);
-            } catch (err) {
-                this.reportErr(new Error(`Send binary message error: ${err}`));
-            }
-        });
-    }
-
-    private reportMsg(msg: string) {
-        this.emit('msg', msg);
-    }
-
-    private reportErr(err: Error) {
-        this.ws?.terminate();
-        this.emit('error', err);
-    }
-
-    private reportClose() {
-        for (const callId in this.sendMessages) {
-            this.sendMessages[callId].reject(new Error('Connection lost'));
-        }
-        this.sendMessages = {};
-        this.emit('close');
     }
 
     updateCGText(fields: Field[]) {
@@ -455,12 +154,5 @@ export class CamOverlayAPI extends EventEmitter {
             auth: this.auth,
             rejectUnauthorized: !this.tlsInsecure,
         };
-    }
-
-    private compareCameraList(cameraList: number[]) {
-        return (
-            this.cameraList.length === cameraList.length &&
-            this.cameraList.every((element, index) => element === cameraList[index])
-        );
     }
 }
