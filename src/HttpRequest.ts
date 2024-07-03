31a3fc7680893bec562a4f42677675c1fcfcb372
Václav Stupka <vaclav.stupka@netrex.cz>
2024-07-03 12:51:12 +0200

    Rewrite HttpRequest.ts

--- src/HttpRequest.ts
+++ src/HttpRequest.ts
@@ -1,14 +1,11 @@
 import { Digest } from './Digest';
 
-export class ErrorStatusCode {
-    constructor(public code: number, public body: Response) {}
-}
 export type HttpRequestOptions = {
     method?: string;
-    protocol?: string;
+    protocol: string;
     host: string;
     port: number;
-    path?: string;
+    path: string;
     auth?: string;
     timeout?: number;
     headers?: Record<string, string>;
@@ -18,17 +15,20 @@ export type HttpRequestOptions = {
 function getURL(options: HttpRequestOptions) {
     const url = new URL(options.protocol + options.host);
 
-    if (options.port) {
-        url.port = options.port.toString();
-    }
-    if (options.path) {
-        url.pathname = options.path;
+    url.port = options.port.toString();
+    url.pathname = options.path;
+
+    if (options.auth !== undefined) {
+        const [user, pass] = options.auth.split(':');
+        url.username = user;
+        url.password = pass;
     }
 
     return url.toString();
 }
+
 function getDigestHeader(options: HttpRequestOptions, digestHeader: string) {
-    if (options.auth == undefined) {
+    if (options.auth === undefined) {
         throw new Error('No credentials found');
     }
     const auth = options.auth.split(':');
@@ -40,32 +40,35 @@ function getDigestHeader(options: HttpRequestOptions, digestHeader: string) {
     return Digest.getAuthHeader(auth[0], auth[1], options.method, options.path, digestHeader);
 }
 
-export async function sendRequest(options: HttpRequestOptions, postData?: Buffer | string, digestHeader?: string) {
+async function sendRequestWithDigest(options: HttpRequestOptions, digestHeader: string, postData?: Buffer | string) {
     const url = getURL(options);
-    if (digestHeader) {
-        options['headers']['Authorization'] = getDigestHeader(options, digestHeader);
-    }
+    options.headers ??= {};
+    options.headers['Authorization'] = getDigestHeader(options, digestHeader);
 
     const controller = new AbortController();
     setTimeout(() => controller.abort(new Error('Request timeout')), options.timeout);
 
-    const req = new Request(url, { body: postData });
-    let res = await fetch(req, { signal: controller.signal });
-
-    if (res.ok) {
-        return res;
-    } else if (res.status == 401) {
-        if (res.headers['www-authenticate'] != undefined && res.headers['www-authenticate'].indexOf('Digest') != -1) {
-            res = await sendRequest(options, postData, res.headers['www-authenticate']);
-            if (res.ok) {
-                return res;
-            }
-        }
-    }
-
-    throw new ErrorStatusCode(res.status, res);
+    const req = new Request(url, { body: postData, method: options.method, headers: options.headers });
+    const res = await fetch(req, { signal: controller.signal });
+    return res;
 }
-export async function getResponse(options: HttpRequestOptions, postData?: Buffer | string, digestHeader?: string) {
-    const res = await sendRequest(options, postData, digestHeader);
-    return res.text();
+
+export async function sendRequest(options: HttpRequestOptions, postData?: Buffer | string) {
+    const url = getURL(options);
+
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(new Error('Request timeout')), options.timeout);
+
+    const req = new Request(url, { body: postData, method: options.method, headers: options.headers });
+    const res = await fetch(req, { signal: controller.signal });
+
+    if (
+        res.status === 401 &&
+        res.headers.has('www-authenticate') &&
+        res.headers.get('www-authenticate')!.indexOf('Digest') !== -1
+    ) {
+        return sendRequestWithDigest(options, res.headers.get('www-authenticate')!, postData);
+    } else {
+        return res;
+    }
 }
