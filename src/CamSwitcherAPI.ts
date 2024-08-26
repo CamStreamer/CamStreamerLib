5509724da4334319b9bada7fdd0967ae903e2bc5
Václav Stupka <vaclav.stupka@netrex.cz>
2024-08-26 09:30:17 +0200

    Sort functions in CamSwitcherAPI into two categories: Playlists and Clips

--- src/CamSwitcherAPI.ts
+++ src/CamSwitcherAPI.ts
@@ -1,5 +1,3 @@
-import * as EventEmitter from 'events';
-
 import { DefaultAgent } from './DefaultAgent';
 import { HttpOptions, IClient, isClient } from './internal/common';
 
@@ -24,15 +22,18 @@ export type TPlaylistInfo = {
     sortIndexOverview: number;
     stream_list: TStreamInfo[];
 };
+export type TPlaylistList = Record<string, TPlaylistInfo>;
+
 export type TClipInfo = {
     niceName: string;
     channel: string;
     keyboard: object;
     sortIndexOverview: number;
 };
-
-export type TPlaylistList = Record<string, TPlaylistInfo>;
 export type TClipList = Record<string, TClipInfo>;
+export type TApiClipType = 'audio' | 'video';
+export type TClipStorage = 'SD_DISK' | 'FLASH';
+
 export type TPlaylistQueue = {
     playlist_queue_list: string[];
 };
@@ -42,55 +43,85 @@ export type TOutputInfo = {
     ws_initial_message: string;
 };
 
-export class CamSwitcherAPI extends EventEmitter {
+export class CamSwitcherAPI {
     private client: IClient;
 
     constructor(options: CamSwitcherAPIOptions | IClient = {}) {
-        super();
-
         if (isClient(options)) {
             this.client = options;
         } else {
             this.client = new DefaultAgent(options);
         }
 
-        EventEmitter.call(this);
     }
+
+    }
+
+    }
+
+    }
+
+    }
+
+    }
+
+    //   ----------------------------------------
+    //                 Playlists
+    //   ----------------------------------------
 
     getPlaylistList(): Promise<TPlaylistList> {
         return this.get('/local/camswitcher/playlists.cgi?action=get');
     }
+    async playlistSwitch(playlistName: string): Promise<void> {
+        await this.get(`/local/camswitcher/playlist_switch.cgi?playlist_name=${playlistName}`);
+    }
+    async playlistQueuePush(playlistName: string): Promise<void> {
+        await this.get(`/local/camswitcher/playlist_queue_push.cgi?playlist_name=${playlistName}`);
+    }
+    async playlistQueueClear(): Promise<void> {
+        await this.get('/local/camswitcher/playlist_queue_clear.cgi');
+    }
+    playlistQueueList(): Promise<TPlaylistQueue> {
+        return this.get('/local/camswitcher/playlist_queue_list.cgi');
+    }
+    async playlistQueuePlayNext(): Promise<void> {
+        await this.get('/local/camswitcher/playlist_queue_play_next.cgi');
+    }
+
+    //   ----------------------------------------
+    //                   Clips
+    //   ----------------------------------------
 
     getClipList(): Promise<TClipList> {
         return this.get('/local/camswitcher/clips.cgi?action=get');
     }
 
-    async playlistSwitch(playlistName: string): Promise<void> {
-        await this.get(`/local/camswitcher/playlist_switch.cgi?playlist_name=${playlistName}`);
+    async addNewClip(file: Buffer, clipType: TApiClipType, storage: TClipStorage, id: string, fileName: string) {
+        const formData = new FormData();
+        formData.append('clip_name', id);
+        formData.append('clip_type', clipType);
+        formData.append('file', file, fileName);
+
+        const path = `/local/camswitcher/clip_upload.cgi?storage=${storage}`;
+
+        const res = await this.client.post(path, formData);
+        const output = (await res.json()) as { status: number; message: string };
+
+        if (output.status !== 200) {
+            throw new Error('Error on camera: ' + output.message);
+        }
     }
 
-    playlistQueueList(): Promise<TPlaylistQueue> {
-        return this.get('/local/camswitcher/playlist_queue_list.cgi');
-    }
-
-    async playlistQueueClear(): Promise<void> {
-        await this.get('/local/camswitcher/playlist_queue_clear.cgi');
-    }
-
-    async playlistQueuePush(playlistName: string): Promise<void> {
-        await this.get(`/local/camswitcher/playlist_queue_push.cgi?playlist_name=${playlistName}`);
-    }
-
-    async playlistQueuePlayNext(): Promise<void> {
-        await this.get('/local/camswitcher/playlist_queue_play_next.cgi');
+    removeClip(id: string, storage: TClipStorage): Promise<{}> {
+        return this.get(`/local/camswitcher/clip_remove.cgi`, { clip_name: id, storage });
     }
 
     getOutputInfo(): Promise<TOutputInfo> {
         return this.get('/local/camswitcher/output_info.cgi');
     }
 
-    async get(path: string): Promise<any> {
-        const res = await this.client.get(path);
+    private async get(path: string, parameters: Record<string, string> = {}): Promise<any> {
+        const res = await this.client.get(path, parameters);
 
         if (res.ok) {
             return await res.json();
