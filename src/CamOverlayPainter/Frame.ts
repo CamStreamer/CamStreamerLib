4577a976477d76d1ecd377ac1e082d39bdf8ba22
Daniel Šimek <daniel.simek@netrex.cz>
2024-11-27 09:31:43 +0100

    Painter/Frame: options for border specification and small refactoring.

--- src/CamOverlayPainter/Frame.ts
+++ src/CamOverlayPainter/Frame.ts
@@ -1,4 +1,4 @@
-import { CamOverlayDrawingAPI, TAlign, TUploadImageResponse } from '../CamOverlayDrawingAPI';
+import { CamOverlayDrawingAPI, TAlign, TCairoCreateResponse, TUploadImageResponse } from '../CamOverlayDrawingAPI';
 import ResourceManager from './ResourceManager';
 
 export type TRgb = [number, number, number];
@@ -6,17 +6,20 @@ export type TRgba = [number, number, number, number];
 export type TTmf = 'TFM_OVERFLOW' | 'TFM_SCALE' | 'TFM_TRUNCATE';
 export type TObjectFitType = 'fill' | 'fit' | 'none';
 export type FrameOptions = {
+    enabled?: boolean;
     x: number;
     y: number;
     width: number;
     height: number;
-    enabled?: boolean;
-
-    bgImage?: string;
     text?: string;
     fontColor?: TRgb;
+    font?: string;
     bgColor?: TRgba;
+    bgImage?: string;
     bgType?: TObjectFitType;
+    borderRadius?: number;
+    borderWidth?: number;
+    borderColor?: TRgba;
 };
 
 export type TFrameInfo = {
@@ -26,41 +29,60 @@ export type TFrameInfo = {
 export type TDrawingCallback = (cod: CamOverlayDrawingAPI, cairo: string, info: TFrameInfo) => Promise<void>;
 
 export default class Frame {
+    protected enabled: boolean;
+
     protected posX: number;
     protected posY: number;
     protected width: number;
     protected height: number;
-    protected enabled: boolean;
 
     private text = '';
+    private fontColor: TRgb;
+    private font?: string | TCairoCreateResponse;
     private align: TAlign = 'A_LEFT';
     private textType: TTmf = 'TFM_OVERFLOW';
-    private fontColor: TRgb;
-    private fontName?: string;
 
     private bgColor?: TRgba;
     private bgImage?: string | TUploadImageResponse;
     private bgType?: TObjectFitType;
 
+    private borderRadius: number;
+    private borderWidth: number;
+    private borderColor: TRgba;
+
     protected children = new Array<Frame>();
 
     constructor(opt: FrameOptions, private customDraw?: TDrawingCallback) {
+        this.enabled = opt.enabled ?? true;
         this.posX = opt.x;
         this.posY = opt.y;
         this.width = opt.width;
         this.height = opt.height;
-        this.enabled = opt.enabled ?? true;
 
         this.setText(opt.text ?? '', 'A_LEFT');
         this.fontColor = opt.fontColor ?? [1.0, 1.0, 1.0];
+        this.font = opt.font;
+
         this.bgColor = opt.bgColor; // RGBA
         this.bgImage = opt.bgImage;
         this.bgType = opt.bgType;
+
+        this.borderRadius = opt.borderRadius ?? 0;
+        this.borderWidth = opt.borderWidth ?? 0;
+        this.borderColor = opt.borderColor ?? [1, 1, 1, 1];
     }
 
     //  -------------------------------
     //      Frame's content setting
     //  -------------------------------
+
+    enable() {
+        this.enabled = true;
+    }
+    disable() {
+        this.enabled = false;
+    }
+
     setFramePosition(x: number, y: number) {
         this.posX = x;
         this.posY = y;
@@ -69,17 +91,23 @@ export default class Frame {
         this.width = width;
         this.height = height;
     }
-    setText(text: string, align: TAlign, textType: TTmf = 'TFM_OVERFLOW', color?: TRgb): void {
+
+    setText(text: string, align: TAlign, textType: TTmf = 'TFM_OVERFLOW', fontColor?: TRgb): void {
         this.text = text;
         this.align = align;
         this.textType = textType;
-        if (color) {
-            this.fontColor = color;
+        if (fontColor) {
+            this.fontColor = fontColor;
         }
     }
-
+    setFontColor(fontColor: TRgb): void {
+        this.fontColor = fontColor;
+    }
     setFont(fontName: string): void {
-        this.fontName = fontName;
+        this.font = fontName;
+    }
+    setFontData(fontData: TCairoCreateResponse): void {
+        this.font = fontData;
     }
     setBgColor(color: TRgba): void {
         this.bgColor = color;
@@ -92,12 +120,28 @@ export default class Frame {
         this.bgImage = imageData;
         this.bgType = type;
     }
+    setBgType(type: TObjectFitType): void {
+        this.bgType = type;
+    }
+
+    setBorderRadius(radius: number): void {
+        this.borderRadius = radius;
+    }
+
+    setBorderWidth(width: number): void {
+        this.borderWidth = width;
+    }
+
+    setBorderColor(color: TRgba): void {
+        this.borderColor = color;
+    }
+
     setCustomDraw(customDraw: TDrawingCallback) {
         this.customDraw = customDraw;
     }
 
     resetFont(): void {
-        this.fontName = undefined;
+        this.font = undefined;
     }
     resetBgColor(): void {
         this.bgColor = undefined;
@@ -115,7 +159,7 @@ export default class Frame {
         this.align = 'A_LEFT';
         this.textType = 'TFM_OVERFLOW';
         this.fontColor = [1.0, 1.0, 1.0];
-        this.fontName = undefined;
+        this.font = undefined;
 
         this.bgColor = undefined;
         this.bgImage = undefined;
@@ -129,65 +173,86 @@ export default class Frame {
     //  ------------------------
     //      Frame displaying
     //  ------------------------
-    enable() {
-        this.enabled = true;
-    }
-    disable() {
-        this.enabled = false;
-    }
 
-    public async displayImage(
+    async displayImage(
         cod: CamOverlayDrawingAPI,
-        rm: ResourceManager,
+        resourceManager: ResourceManager,
         cairo: string,
         ppX: number,
         ppY: number,
-        scale = 1
+        scale = 1.0
     ) {
         if (this.enabled) {
             ppX += this.posX;
             ppY += this.posY;
-            await this.displayOwnImage(cod, rm, cairo, ppX, ppY, scale);
+
+            // Resources have to be prepared before drawing to avoid async problems
+            await this.prepareResources(resourceManager);
+
+            await cod.cairo('cairo_save', cairo);
+            await this.clipDrawing(cod, cairo, scale, ppX, ppY);
+
+            await this.displayOwnImage(cod, cairo, ppX, ppY, scale);
             for (const child of this.children) {
-                await child.displayImage(cod, rm, cairo, ppX, ppY, scale);
+                await child.displayImage(cod, resourceManager, cairo, ppX, ppY, scale);
             }
+
+            await cod.cairo('cairo_restore', cairo);
         }
     }
-    protected async displayOwnImage(
-        cod: CamOverlayDrawingAPI,
-        rm: ResourceManager,
-        cairo: string,
-        ppX: number,
-        ppY: number,
-        scale: number
-    ) {
-        if (this.enabled) {
-            const promises = new Array<Promise<unknown>>();
 
-            if (this.fontName !== undefined) {
-                const fontData = await rm.font(this.fontName);
-                promises.push(cod.cairo('cairo_set_font_face', cairo, fontData.var));
-            } else {
-                promises.push(cod.cairo('cairo_set_font_face', cairo, 'NULL'));
-            }
-            if (this.bgColor !== undefined) {
-                promises.push(this.drawFrame(cod, cairo, scale, ppX, ppY));
-            }
-            if (this.bgImage !== undefined) {
-                promises.push(this.drawImage(cod, rm, cairo, scale, ppX, ppY));
-            }
-            if (this.text) {
-                promises.push(this.drawText(cod, cairo, scale, ppX, ppY));
-            }
-            if (this.customDraw) {
-                promises.push(cod.cairo('cairo_identity_matrix', cairo));
-                promises.push(cod.cairo('cairo_translate', cairo, scale * ppX, scale * ppY));
-                promises.push(cod.cairo('cairo_scale', cairo, scale, scale));
-                promises.push(this.customDraw(cod, cairo, { width: this.width, height: this.height }));
-            }
-
-            await Promise.all(promises);
+    private async prepareResources(resourceManager: ResourceManager) {
+        if (typeof this.bgImage === 'string') {
+            this.bgImage = await resourceManager.image(this.bgImage);
         }
+        if (typeof this.font === 'string') {
+            this.font = await resourceManager.font(this.font);
+        }
+    }
+
+    protected async displayOwnImage(cod: CamOverlayDrawingAPI, cairo: string, ppX: number, ppY: number, scale: number) {
+        if (!this.enabled) {
+            return;
+        }
+
+        const promises = new Array<Promise<unknown>>();
+        if (this.font !== undefined && typeof this.font !== 'string') {
+            promises.push(cod.cairo('cairo_set_font_face', cairo, this.font.var));
+        } else {
+            promises.push(cod.cairo('cairo_set_font_face', cairo, 'NULL'));
+        }
+
+        if (this.bgColor !== undefined) {
+            promises.push(this.drawFrame(cod, cairo, scale, ppX, ppY));
+        }
+        if (this.bgImage !== undefined) {
+            promises.push(this.drawImage(cod, cairo, scale, ppX, ppY));
+        }
+        if (this.borderWidth > 0) {
+            promises.push(this.drawBorder(cod, cairo, scale, ppX, ppY));
+        }
+        if (this.text) {
+            promises.push(this.drawText(cod, cairo, scale, ppX, ppY));
+        }
+        if (this.customDraw) {
+            promises.push(cod.cairo('cairo_identity_matrix', cairo));
+            promises.push(cod.cairo('cairo_translate', cairo, scale * ppX, scale * ppY));
+            promises.push(cod.cairo('cairo_scale', cairo, scale, scale));
+            promises.push(this.customDraw(cod, cairo, { width: this.width, height: this.height }));
+        }
+
+        await Promise.all(promises);
+    }
+
+    private async clipDrawing(cod: CamOverlayDrawingAPI, cairo: string, scale: number, ppX: number, ppY: number) {
+        if (this.borderRadius === 0) {
+            return;
+        }
+
+        await Promise.all([
+            this.drawRectPath(cod, cairo, scale, ppX, ppY, this.borderRadius),
+            cod.cairo('cairo_clip', cairo),
+        ]);
     }
 
     private async drawFrame(cod: CamOverlayDrawingAPI, cairo: string, scale: number, ppX: number, ppY: number) {
@@ -210,21 +275,18 @@ export default class Frame {
             ];
             await Promise.all(promises);
         } else {
-            throw new Error('Colour of this frame is undefined.');
+            throw new Error('Color of the frame is undefined.');
         }
     }
-    private async drawImage(
-        cod: CamOverlayDrawingAPI,
-        rm: ResourceManager,
-        cairo: string,
-        scale: number,
-        ppX: number,
-        ppY: number
-    ) {
-        const imageData = typeof this.bgImage === 'string' ? await rm.image(this.bgImage) : this.bgImage!;
-        const bgImage = imageData.var;
-        const bgWidth = imageData.width;
-        const bgHeight = imageData.height;
+
+    private async drawImage(cod: CamOverlayDrawingAPI, cairo: string, scale: number, ppX: number, ppY: number) {
+        if (this.bgImage === undefined || typeof this.bgImage === 'string') {
+            return;
+        }
+
+        const bgImage = this.bgImage.var;
+        const bgWidth = this.bgImage.width;
+        const bgHeight = this.bgImage.height;
 
         const promises = new Array<Promise<unknown>>();
         promises.push(cod.cairo('cairo_identity_matrix', cairo));
@@ -246,6 +308,7 @@ export default class Frame {
         promises.push(cod.cairo('cairo_paint', cairo));
         await Promise.all(promises);
     }
+
     private async drawText(cod: CamOverlayDrawingAPI, cairo: string, scale: number, ppX: number, ppY: number) {
         const promises = [
             cod.cairo('cairo_identity_matrix', cairo),
@@ -262,6 +325,61 @@ export default class Frame {
             ),
         ];
         await Promise.all(promises);
+    }
+
+    private async drawBorder(cod: CamOverlayDrawingAPI, cairo: string, scale: number, ppX: number, ppY: number) {
+        await Promise.all([
+            this.drawRectPath(cod, cairo, scale, ppX, ppY, this.borderRadius),
+            cod.cairo(
+                'cairo_set_source_rgba',
+                cairo,
+                this.borderColor[0],
+                this.borderColor[1],
+                this.borderColor[2],
+                this.borderColor[3]
+            ),
+            cod.cairo('cairo_set_line_width', cairo, this.borderWidth),
+            cod.cairo('cairo_stroke', cairo),
+        ]);
+    }
+
+    private async drawRectPath(
+        cod: CamOverlayDrawingAPI,
+        cairo: string,
+        scale: number,
+        ppX: number,
+        ppY: number,
+        radius: number
+    ) {
+        if (radius === 0) {
+            return await Promise.all([
+                cod.cairo('cairo_identity_matrix', cairo),
+                cod.cairo('cairo_translate', cairo, scale * ppX, scale * ppY),
+                cod.cairo('cairo_scale', cairo, scale, scale),
+                cod.cairo('cairo_rectangle', cairo, 0, 0, this.width, this.height),
+            ]);
+        } else {
+            const degrees = Math.PI / 180;
+            return await Promise.all([
+                cod.cairo('cairo_identity_matrix', cairo),
+                cod.cairo('cairo_translate', cairo, scale * ppX, scale * ppY),
+                cod.cairo('cairo_scale', cairo, scale, scale),
+                cod.cairo('cairo_new_sub_path', cairo),
+                cod.cairo('cairo_arc', cairo, this.width - radius, radius, radius, -90 * degrees, 0 * degrees),
+                cod.cairo(
+                    'cairo_arc',
+                    cairo,
+                    this.width - radius,
+                    this.height - radius,
+                    radius,
+                    0 * degrees,
+                    90 * degrees
+                ),
+                cod.cairo('cairo_arc', cairo, radius, this.height - radius, radius, 90 * degrees, 180 * degrees),
+                cod.cairo('cairo_arc', cairo, radius, radius, radius, 180 * degrees, 270 * degrees),
+                cod.cairo('cairo_close_path', cairo),
+            ]);
+        }
     }
 }
 
