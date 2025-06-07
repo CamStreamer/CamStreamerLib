3ec2c72a7a9f17d071ee75f1fc28915372a2d0d1
Helena Beranova <helena.beranova@netrex.cz>
2025-06-07 16:26:36 +0200

    update: move all cameraVapix types to separate file

--- src/CameraVapix.ts
+++ src/CameraVapix.ts
@@ -2,106 +2,22 @@ import * as prettifyXml from 'prettify-xml';
 import { parseStringPromise } from 'xml2js';
 import { WritableStream } from 'node:stream/web';
 
-import { HttpOptions, IClient, isClient, responseStringify } from './internal/common';
+import { IClient, isClient, responseStringify } from './internal/common';
 import { DefaultAgent } from './DefaultAgent';
 
-export type CameraVapixOptions = HttpOptions;
-
-export type TApplicationList = {
-    reply: {
-        $: { result: string };
-        application: {
-            $: TApplication;
-        }[];
-    };
-};
-
-export type TApplication = {
-    Name: string;
-    NiceName: string;
-    Vendor: string;
-    Version: string;
-    ApplicationID?: string;
-    License: string;
-    Status: string;
-    ConfigurationPage?: string;
-    VendorHomePage?: string;
-    LicenseName?: string;
-};
-
-export type TGuardTour = {
-    id: string;
-    camNbr: unknown;
-    name: string;
-    randomEnabled: unknown;
-    running: string;
-    timeBetweenSequences: unknown;
-    tour: {
-        moveSpeed: unknown;
-        position: unknown;
-        presetNbr: unknown;
-        waitTime: unknown;
-        waitTimeViewType: unknown;
-    }[];
-};
-
-export type TAudioSampleRates = {
-    sampleRate: number;
-    bitRates: number[];
-};
-
-export type TSDCardInfo = {
-    available: boolean;
-    totalSize: number;
-    freeSize: number;
-};
-
-export type TPtzOverview = Record<number, { id: number; name: string }[]>;
-export type TCameraPTZItem = {
-    name: string;
-    id: number;
-    data: TCameraPTZItemData;
-};
-export type TCameraPTZItemData = {
-    pan?: number;
-    tilt?: number;
-    zoom?: number;
-};
-
-export type TAudioDeviceSignalingChannelType = {
-    id: string;
-    gain: number;
-    mute: boolean;
-};
-export type TAudioDeviceSignalingType = {
-    id: string;
-    powerType: string;
-    channels: TAudioDeviceSignalingChannelType[];
-};
-export type TAudioDeviceConnectionType = {
-    id: string;
-    signalingTypeSelected: string;
-    signalingTypes: TAudioDeviceSignalingType[];
-};
-export type TAudioDeviceInputOutput = {
-    id: string;
-    name: string;
-    enabled: boolean;
-    connectionTypes: TAudioDeviceConnectionType[];
-    connectionTypeSelected: string;
-};
-export type TAudioDevice = {
-    id: string;
-    name: string;
-    inputs: TAudioDeviceInputOutput[];
-    outputs: TAudioDeviceInputOutput[];
-};
-export type TAudioDeviceFromRequest = {
-    id: string;
-    name: string;
-    inputs?: TAudioDeviceInputOutput[];
-    outputs?: TAudioDeviceInputOutput[];
-};
+import {
+    CameraVapixOptions,
+    TApplicationList,
+    TApplication,
+    TGuardTour,
+    TAudioSampleRates,
+    TSDCardInfo,
+    TPtzOverview,
+    TCameraPTZItem,
+    TCameraPTZItemData,
+    TAudioDevice,
+    TAudioDeviceFromRequest,
+} from './types/CameraVapix';
 
 export class CameraVapix {
     private client: IClient;
