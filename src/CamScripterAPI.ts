7ef51b3655d10de520927849e4527de8946368aa
Helena Beranova <helena.beranova@netrex.cz>
2025-09-01 16:49:04 +0200

    unify methods response validation

--- src/CamScripterAPI.ts
+++ src/CamScripterAPI.ts
@@ -2,6 +2,9 @@ import { IClient, TResponse } from './internal/types';
 import { responseStringify } from './internal/utils';
 
 import {
+    cameraTimeResponseSchema,
+    camscripterApiResponseSchema,
+    nodeStateSchema,
     packageInfoListSchema,
     storageSchema,
     TNodeState,
@@ -11,21 +14,25 @@ import {
 } from './types/CamScripterAPI';
 import { networkCameraListSchema, TNetworkCamera } from './types/common';
 
+export const BASE_URL = '/local/camscripter/package';
 export class CamScripterAPI<Client extends IClient<TResponse> = IClient<TResponse>> {
     constructor(public client: Client) {}
 
+    static getProxyUrlPath = () => `${BASE_URL}/proxy.cgi`;
+
     async checkCameraTime(): Promise<boolean> {
-        return (await this.get('/local/camscripter/camera_time.cgi')).state;
+        const data = await this.get(`${BASE_URL}/camera_time.cgi`);
+        return cameraTimeResponseSchema.parse(data).state;
     }
 
     async getStorageInfo(): Promise<TStorage> {
-        const data = await this.get(`/local/camscripter/package/get_storage.cgi`);
+        const data = await this.get(`${BASE_URL}/get_storage.cgi`);
         return storageSchema.parse(data);
     }
 
     async getNetworkCameraList(): Promise<TNetworkCamera[]> {
-        const response = await this.get('/local/camscripter/network_camera_list.cgi');
-        return networkCameraListSchema.parse(response.camera_list);
+        const data = await this.get(`${BASE_URL}/network_camera_list.cgi`);
+        return networkCameraListSchema.parse(data.camera_list);
     }
 
     //   ----------------------------------------
@@ -33,24 +40,28 @@ export class CamScripterAPI<Client extends IClient<TResponse> = IClient<TRespons
     //   ----------------------------------------
 
     async getPackageList(): Promise<TPackageInfoList> {
-        const data = await this.get('/local/camscripter/package/list.cgi');
+        const data = await this.get(`${BASE_URL}/list.cgi`);
         return packageInfoListSchema.parse(data);
     }
 
-    async installPackages(formData: FormData, storage: TStorageType): Promise<void> {
-        await this.post(`/local/camscripter/package/install.cgi?storage=${storage}`, formData);
+    async installPackages(formData: FormData, storage: TStorageType) {
+        const data = await this.post(`${BASE_URL}/install.cgi?storage=${storage}`, formData);
+        return camscripterApiResponseSchema.parse(data);
     }
 
-    async uninstallPackage(packageId: string): Promise<void> {
-        await this.get(`/local/camscripter/package/remove.cgi?package_name=${packageId}`);
+    async uninstallPackage(packageId: string) {
+        const data = await this.get(`${BASE_URL}/remove.cgi?package_name=${packageId}`);
+        return camscripterApiResponseSchema.parse(data);
     }
 
-    async importSettings(packageId: string, formData: FormData): Promise<void> {
-        await this.post(`/local/camscripter/package/data.cgi?action=IMPORT&package_name=${packageId}`, formData);
+    async importSettings(packageId: string, formData: FormData) {
+        const data = await this.post(`${BASE_URL}/data.cgi?action=IMPORT&package_name=${packageId}`, formData);
+        return camscripterApiResponseSchema.parse(data);
     }
 
-    async exportSettings(packageId: string, formData: FormData): Promise<void> {
-        await this.post(`/local/camscripter/package/data.cgi?action=EXPORT&package_name=${packageId}`, formData);
+    async exportSettings(packageId: string, formData: FormData) {
+        const data = await this.post(`${BASE_URL}/data.cgi?action=EXPORT&package_name=${packageId}`, formData);
+        return camscripterApiResponseSchema.parse(data);
     }
 
     //   ----------------------------------------
@@ -58,11 +69,13 @@ export class CamScripterAPI<Client extends IClient<TResponse> = IClient<TRespons
     //   ----------------------------------------
 
     async getNodejsStatus(): Promise<TNodeState> {
-        return (await this.get('/local/camscripter/diagnostics.cgi')).node_state;
+        const data = await this.get(`${BASE_URL}/diagnostics.cgi`);
+        return nodeStateSchema.parse(data);
     }
 
-    async installNodejs(storage: TStorageType): Promise<void> {
-        await this.get(`/local/camscripter/node_update.cgi?storage=${storage}`);
+    async installNodejs(storage: TStorageType) {
+        const data = await this.get(`${BASE_URL}/node_update.cgi?storage=${storage}`);
+        return camscripterApiResponseSchema.parse(data);
     }
 
     private async get(path: string, params?: Record<string, string>): Promise<any> {
