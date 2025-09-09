f1b354b2d8adb431480fd2511db0dffcdcbfd92a
Helena Beranova <helena.beranova@netrex.cz>
2025-09-09 13:18:18 +0200

    fix: xml parsing array fix

--- src/VapixAPI.ts
+++ src/VapixAPI.ts
@@ -497,8 +497,12 @@ export class VapixAPI<Client extends IClient<TResponse> = IClient<TResponse>> {
             allowBooleanAttributes: true,
         });
         const result = parser.parse(xml);
+        const apps = [];
+        for (let i = 0; i < result.reply.application.length; i++) {
+            apps.push(result.reply.application[i].$);
+        }
 
-        return (result.reply.application as z.infer<typeof applicationSchema>[]).map((app) => {
+        return apps.map((app) => {
             return {
                 ...app,
                 appId: APP_IDS.find((id) => id.toLowerCase() === app.Name.toLowerCase()) ?? null,
