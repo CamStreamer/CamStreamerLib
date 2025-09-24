7ebd46fdfdb0ad5effb2d2764c415ddeac6abacf
Helena Beranova <helena.beranova@netrex.cz>
2025-09-24 16:57:20 +0200

    export events

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,2 +1,4 @@
 export { DefaultClient } from './DefaultClient';
 export { WsClient } from './WsClient';
+export { AxisCameraStationEvents } from '../events/AxisCameraStationEvents';
+export * from '../events/GenetecAgent';
