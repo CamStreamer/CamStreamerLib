f80f58c892dc4f3a9843ed8ac1152fa0c72a3237
Daniel Šimek <daniel.simek@netrex.cz>
2025-09-25 12:01:58 +0200

    HttpServer added to node exports.

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,4 +1,5 @@
-export { DefaultClient } from './DefaultClient';
-export { WsClient } from './WsClient';
-export { AxisCameraStationEvents } from '../events/AxisCameraStationEvents';
+export * from './DefaultClient';
+export * from './WsClient';
+export * from './HttpServer';
+export * from '../events/AxisCameraStationEvents';
 export * from '../events/GenetecAgent';
