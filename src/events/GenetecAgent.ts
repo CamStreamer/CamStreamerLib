f9bcb33589057e2045b75285ecbe054109dac541
Daniel Šimek <daniel.simek@netrex.cz>
2025-09-25 15:17:14 +0200

    GenetecAgent: better error handling and url encoding.

--- src/events/GenetecAgent.ts
+++ src/events/GenetecAgent.ts
@@ -1,40 +1,31 @@
 import { z } from 'zod';
 import { responseStringify, pad } from '../internal/utils';
 
-const ACTION = 'AddCameraBookmark';
-const GET_CAMERAS_URL = 'report/EntityConfiguration?q=EntityTypes@Camera';
-const GET_CAMERAS_DETAILS_URL = '/entity?q=';
+const successResponseSchema = z.object({
+    Rsp: z.object({
+        Status: z.literal('Ok'),
+    }),
+});
 
 const cameraGuidsResponseSchema = z.object({
     Rsp: z.object({
-        Status: z.string(),
+        Status: z.literal('Ok'),
         Result: z.array(z.object({ Guid: z.string() })),
     }),
 });
-
 export type TCameraGuidsResponse = z.infer<typeof cameraGuidsResponseSchema>;
 
-const connectionResponseSchema = z.object({
-    Rsp: z.object({
-        Status: z.string(),
-    }),
-});
-
-export type TConnectionResponse = z.infer<typeof connectionResponseSchema>;
-
-export const cameraDetailSchema = z.object({
+const cameraDetailSchema = z.object({
     Guid: z.string().optional(),
     Name: z.string().optional(),
     EntityType: z.string().optional(),
 });
-
-export const cameraDetailsResponseSchema = z.object({
+const cameraDetailsResponseSchema = z.object({
     Rsp: z.object({
-        Status: z.string(),
+        Status: z.literal('Ok'),
         Result: z.union([z.array(cameraDetailSchema), cameraDetailSchema]),
     }),
 });
-
 export type TCameraDetailsResponse = z.infer<typeof cameraDetailsResponseSchema>;
 export type TCameraDetail = z.infer<typeof cameraDetailSchema>;
 
@@ -73,30 +64,45 @@ export class GenetecAgent {
         this.credentials = btoa(`${this.settings.user};${this.settings.appId}:${this.settings.pass}`);
     }
 
-    async checkConnection(): Promise<TConnectionResponse> {
+    async checkConnection() {
         const requestOptions = this.getRequestOptions('GET');
-        const res = await this.fetchWithTimeout(`${this.baseUrl}/`, requestOptions);
+
+        const res = await this.fetchWithTimeout(new URL(this.baseUrl), requestOptions);
 
         if (!res.ok) {
             throw new Error(await responseStringify(res));
         }
-        return connectionResponseSchema.parse(await res.json());
+        const responseBody = await res.text();
+        const result = await successResponseSchema.safeParseAsync(JSON.parse(responseBody));
+        if (!result.success) {
+            throw new Error(
+                'Genetec connection test failed: ' + JSON.stringify(result.error.issues) + '\n' + responseBody
+            );
+        }
     }
 
     async getAllCameraGuids(): Promise<TCameraGuidsResponse> {
         const requestOptions = this.getRequestOptions('GET');
-        const res = await this.fetchWithTimeout(`${this.baseUrl}/${GET_CAMERAS_URL}`, requestOptions);
+        const url = new URL(`${this.baseUrl}/report/EntityConfiguration`);
+        url.searchParams.set('q', 'EntityTypes@Camera');
+        const res = await this.fetchWithTimeout(url, requestOptions);
 
         if (!res.ok) {
             throw new Error(await responseStringify(res));
         }
 
-        return cameraGuidsResponseSchema.parse(await res.json());
+        const responseBody = await res.text();
+        const result = await cameraGuidsResponseSchema.safeParseAsync(JSON.parse(responseBody));
+        if (!result.success) {
+            throw new Error(
+                'Genetec get camera guids failed: ' + JSON.stringify(result.error.issues) + '\n' + responseBody
+            );
+        }
+        return result.data;
     }
 
     async getCameraDetails(guids: { Guid: string }[], parameters: TParams): Promise<TCameraDetail[]> {
         const params = parameters.join(',');
-        let camerasGuids: string[] = [];
         const requestOptions = this.getRequestOptions('GET');
         const allCameras = [] as TCameraDetail[];
 
@@ -105,23 +111,23 @@ export class GenetecAgent {
             const chunk = guids.slice(0, chunkSize);
             guids.splice(0, chunkSize);
 
-            camerasGuids = chunk.map((item) => item.Guid);
-            const camerasDetailsUrl = [];
-
-            for (const guid of camerasGuids) {
-                camerasDetailsUrl.push(`entity=${guid},${params}`);
-            }
-
-            const res = await this.fetchWithTimeout(
-                `${this.baseUrl}/${GET_CAMERAS_DETAILS_URL}${camerasDetailsUrl.join(',')}`,
-                requestOptions
-            );
+            const url = new URL(`${this.baseUrl}/entity`);
+            url.searchParams.set('q', chunk.map((item) => `entity=${item.Guid},${params}`).join(','));
+            const res = await this.fetchWithTimeout(url, requestOptions);
 
             if (!res.ok) {
                 throw new Error(await responseStringify(res));
             }
 
-            const data = cameraDetailsResponseSchema.parse(await res.json());
+            const responseBody = await res.text();
+            const result = await cameraDetailsResponseSchema.safeParseAsync(JSON.parse(responseBody));
+            if (!result.success) {
+                throw new Error(
+                    'Genetec get camera details failed: ' + JSON.stringify(result.error.issues) + '\n' + responseBody
+                );
+            }
+
+            const data = result.data;
             const resultArray = Array.isArray(data.Rsp.Result) ? data.Rsp.Result : [data.Rsp.Result];
             allCameras.push(...resultArray);
         }
@@ -129,24 +135,29 @@ export class GenetecAgent {
         return allCameras;
     }
 
-    async sendBookmark(guids: string[], bookmarkText: string): Promise<Response> {
-        const cameraEntitiesUrl: string[] = [];
+    async sendBookmark(guids: string[], bookmarkText: string) {
         const timeStamp = this.getTimeStamp();
         const requestOptions = this.getRequestOptions('POST');
 
-        for (const guid of guids) {
-            cameraEntitiesUrl.push(`${ACTION}(${guid},${timeStamp},${bookmarkText})`);
-        }
-
-        const res = await this.fetchWithTimeout(
-            `${this.baseUrl}/action?q=${cameraEntitiesUrl.join(',')}`,
-            requestOptions
+        const url = new URL(`${this.baseUrl}/action`);
+        url.searchParams.set(
+            'q',
+            guids.map((guid) => `AddCameraBookmark(${guid},${timeStamp},${bookmarkText})`).join(',')
         );
+
+        const res = await this.fetchWithTimeout(url, requestOptions);
 
         if (!res.ok) {
             throw new Error(await responseStringify(res));
         }
-        return res;
+
+        const responseBody = await res.text();
+        const result = await successResponseSchema.safeParseAsync(JSON.parse(responseBody));
+        if (!result.success) {
+            throw new Error(
+                'Genetec send bookmark failed: ' + JSON.stringify(result.error.issues) + '\n' + responseBody
+            );
+        }
     }
 
     private getRequestOptions(method: string): RequestInit {
@@ -173,7 +184,7 @@ export class GenetecAgent {
         return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${miliSeconds}Z`;
     }
 
-    private async fetchWithTimeout(url: string, options: RequestInit): Promise<Response> {
+    private async fetchWithTimeout(url: URL, options: RequestInit): Promise<Response> {
         const controller = new AbortController();
         const timeoutId = setTimeout(() => controller.abort(), this.settings.timeout);
         try {
