5c4b201cb4c5cf05993887081ead3be6e8249beb
peter.mento <peter.mento@netrex.cz>
2025-12-08 16:19:54 +0100

    youtube schema

--- src/types/CamStreamerAPI/youtubeSchema.ts
+++ src/types/CamStreamerAPI/youtubeSchema.ts
@@ -3,4 +3,19 @@ import { streamCommonSchema } from './streamCommonTypes';
 
 export const youtubeSchema = streamCommonSchema.extend({
     type: z.literal('youtube'),
+    description: z.string().optional(),
+    playlist: z.string().optional(),
+    tags: z.array(z.string()),
+    notificationEmail: z.array(z.string().email()).optional(),
+    streamPrivacy: z.union([z.literal('public'), z.literal('unlisted'), z.literal('private')]),
+    latency: z.union([z.literal('normal'), z.literal('low'), z.literal('ultra_low')]),
+    afterEndStatus: z.union([z.literal('no_change'), z.literal('public'), z.literal('unlisted'), z.literal('private')]),
+    cameraLedStatus: z.boolean(),
+    dvr: z.boolean(),
+    saveToSdCard: z.boolean(),
+    statusCameraLed: z.boolean(),
+    statusCameraOutput: z.string(),
+    hasWatchdogs: z.boolean(),
+    countdown: z.boolean(),
+    enableManualControl: z.boolean(),
 });
