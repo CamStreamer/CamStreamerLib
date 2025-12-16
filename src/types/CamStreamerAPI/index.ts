8aa42001ec7c1a7e21ff251011011c86465eef78
Jan Tichavsky <jan.tichavsky@netrex.cz>
2025-12-16 10:54:59 +0100

    reexport gamechanger schema

--- src/types/CamStreamerAPI/index.ts
+++ src/types/CamStreamerAPI/index.ts
@@ -4,6 +4,7 @@ export * from './streamCommonTypes';
 export * from './facebookSchema';
 export * from './mpegDvbSchema';
 export * from './rtmpSchema';
+export * from './gameChangerSchema';
 export * from './sdCardSchema';
 export * from './windySchema';
 export * from './youtubeSchema';
