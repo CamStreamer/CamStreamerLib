bf9d28b1089123fccd4fe1fe07ef453b2d4fa72b
peter.mento <peter.mento@netrex.cz>
2025-12-18 09:38:14 +0100

    adjusted facebookSchema

--- src/types/CamStreamerAPI/facebookSchema.ts
+++ src/types/CamStreamerAPI/facebookSchema.ts
@@ -1,6 +1,23 @@
 import z from 'zod';
 import { streamCommonSchema } from './streamCommonTypes';
 
+const timelinePost = z.object({
+    postLocation: z.literal('timeline'),
+    streamPrivacy: z.union([z.literal('public'), z.literal('friends'), z.literal('only_me')]),
+});
+
+const pagePost = z.object({
+    postLocation: z.literal('page'),
+    page: z.string(),
+});
+
 export const facebookSchema = streamCommonSchema.extend({
     type: z.literal('facebook'),
+    description: z.string().optional(),
+    deleteAfterEnd: z.boolean(),
+    saveToSdCard: z.boolean(),
+    statusCameraLed: z.boolean(),
+    statusCameraOutput: z.string().nullable(),
+    countdown: z.boolean(),
+    post: z.discriminatedUnion('postLocation', [timelinePost, pagePost]),
 });
