f53bd1a555c08b29bdf3200dd668f0136e8da319
Helena Beranova <helena.beranova@netrex.cz>
2026-01-08 09:09:36 +0100

    chore: rename id to streamId, type to platform

--- src/types/CamStreamerAPI/streamsSchema.ts
+++ src/types/CamStreamerAPI/streamsSchema.ts
@@ -7,53 +7,53 @@ export const commonRtmpSchema = streamCommonSchema.extend({
 });
 
 export const churchSchema = streamCommonSchema.extend({
-    type: z.literal('church'),
+    platform: z.literal('church'),
 });
 export const daCastSchema = streamCommonSchema.extend({
-    type: z.literal('da_cast'),
+    platform: z.literal('da_cast'),
 });
 export const dailymotionSchema = streamCommonSchema.extend({
-    type: z.literal('dailymotion'),
+    platform: z.literal('dailymotion'),
 });
 export const gameChangerSchema = commonRtmpSchema.extend({
-    type: z.literal('game_changer'),
+    platform: z.literal('game_changer'),
 });
 export const hlsPullSchema = streamCommonSchema.extend({
-    type: z.literal('hls_pull'),
+    platform: z.literal('hls_pull'),
 });
 export const hlsPushSchema = streamCommonSchema.extend({
-    type: z.literal('hls_push'),
+    platform: z.literal('hls_push'),
 });
 export const ibmSchema = streamCommonSchema.extend({
-    type: z.literal('ibm'),
+    platform: z.literal('ibm'),
 });
 export const mpegDvbSchema = streamCommonSchema.extend({
-    type: z.literal('mpeg_dvb'),
+    platform: z.literal('mpeg_dvb'),
 });
 export const microsoftAzureSchema = streamCommonSchema.extend({
-    type: z.literal('microsoft_azure'),
+    platform: z.literal('microsoft_azure'),
 });
 export const microsoftStreamSchema = streamCommonSchema.extend({
-    type: z.literal('microsoft_stream'),
+    platform: z.literal('microsoft_stream'),
 });
 export const rtmpSchema = commonRtmpSchema.extend({
-    type: z.literal('rtmp'),
+    platform: z.literal('rtmp'),
 });
 export const sdCardSchema = streamCommonSchema.extend({
-    type: z.literal('sd_card'),
+    platform: z.literal('sd_card'),
 });
 export const srtSchema = streamCommonSchema.extend({
-    type: z.literal('srt'),
+    platform: z.literal('srt'),
 });
 export const twitchSchema = streamCommonSchema.extend({
-    type: z.literal('twitch'),
+    platform: z.literal('twitch'),
 });
 export const vimeoSchema = streamCommonSchema.extend({
-    type: z.literal('vimeo'),
+    platform: z.literal('vimeo'),
 });
 export const wowzaSchema = streamCommonSchema.extend({
-    type: z.literal('wowza'),
+    platform: z.literal('wowza'),
 });
 export const youtubeRtmpSchema = commonRtmpSchema.extend({
-    type: z.literal('youtube_rtmp'),
+    platform: z.literal('youtube_rtmp'),
 });
--- src/types/CamStreamerAPI/youtubeSchema.ts
+++ src/types/CamStreamerAPI/youtubeSchema.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 import { streamCommonSchema } from './streamCommonTypes';
 
 export const youtubeSchema = streamCommonSchema.extend({
-    type: z.literal('youtube'),
+    platform: z.literal('youtube'),
     description: z.string().optional(),
     playlist: z.string().optional(),
     tags: z.array(z.string()),
