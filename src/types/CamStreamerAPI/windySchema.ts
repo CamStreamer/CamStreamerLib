9990a475b8180bbfd3f0d7d920b2de986f185518
peter.mento <peter.mento@netrex.cz>
2026-04-24 08:13:22 +0200

    revert: mediaServerUrl in Windy

--- src/types/CamStreamerAPI/windySchema.ts
+++ src/types/CamStreamerAPI/windySchema.ts
@@ -11,7 +11,6 @@ export const windySchema = streamCommonSchema.extend({
     direction: z.enum(['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']).nullable(),
     position: z.union([z.literal('fix'), z.literal('rotating'), z.literal('controllable')]),
     webPageUrl: z.string(),
-    mediaServerUrl: z.string().nullable(),
 });
 
 export type TWindyDirection = z.infer<typeof windySchema>['direction'];
