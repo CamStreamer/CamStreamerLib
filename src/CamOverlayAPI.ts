c96ce4f7fc69fb970971335b1c9986ae97473a34
Jakub Pejcha <jakub.pejcha@netrex.cz>
2026-05-04 10:38:53 +0200

    Adding api check dummy api

--- src/CamOverlayAPI.ts
+++ src/CamOverlayAPI.ts
@@ -36,6 +36,10 @@ export class CamOverlayAPI<Client extends IClient<TResponse, any>> {
         return proxyParams ? new ProxyClient(this.client, proxyParams) : this.client;
     }
 
+    async checkAPIAvailable(options?: THttpRequestOptions) {
+        await this._getJson(`${BASE_PATH}/api_check.cgi`, undefined, options);
+    }
+
     async checkCameraTime(options?: THttpRequestOptions) {
         const res = await this._getJson(`${BASE_PATH}/camera_time.cgi`, undefined, options);
         return z.boolean().parse(res.state);
