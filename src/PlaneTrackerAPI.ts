3bd48205fcf78afcb3bd81a0df582acc9808d771
Jan Tichavsky <jan.tichavsky@netrex.cz>
2026-05-04 10:38:54 +0200

    v4.0.0-beta.125

--- src/PlaneTrackerAPI.ts
+++ src/PlaneTrackerAPI.ts
@@ -72,6 +72,15 @@ export class PlaneTrackerAPI<Client extends IClient<TResponse, any>> extends Bas
         return wsAliasResponseSchema.parse(await res.json());
     }
 
+    postJson = (
+        path: string,
+        data: string | Parameters<Client['post']>[0]['data'],
+        parameters?: TParameters,
+        options?: THttpRequestOptions
+    ) => {
+        return this._postJsonEncoded(path, data, parameters, options);
+    };
+
     //   ----------------------------------------
     //                 Calibration
     //   ----------------------------------------
