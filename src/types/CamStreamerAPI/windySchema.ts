85454ab57fffc95a31c8bb1be0daa9ecb17a0503
Helena Beranova <helena.beranova@netrex.cz>
2026-05-04 10:38:55 +0200

    csApi: add base windy schema

--- src/types/CamStreamerAPI/windySchema.ts
+++ src/types/CamStreamerAPI/windySchema.ts
@@ -3,4 +3,13 @@ import { streamCommonSchema } from './streamCommonTypes';
 
 export const windySchema = streamCommonSchema.extend({
     platform: z.literal('windy'),
+    locationLat: z.number(),
+    locationLon: z.number(),
+    locationName: z.string(),
+    direction: z.enum(['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']).nullable(),
+    position: z.union([z.literal('fix'), z.literal('rotating'), z.literal('controllable')]),
+    webPageUrl: z.string(),
 });
+
+export type TWindyDirection = z.infer<typeof windySchema>['direction'];
+export type TWindyPosition = z.infer<typeof windySchema>['position'];
