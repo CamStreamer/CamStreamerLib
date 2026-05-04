3e063a10355e242a39c21e5c6c94663af48e04b5
peter.mento <peter.mento@netrex.cz>
2026-05-04 10:38:52 +0200

    removed duplicate prop

--- src/types/CamStreamerAPI/youtubeSchema.ts
+++ src/types/CamStreamerAPI/youtubeSchema.ts
@@ -10,7 +10,6 @@ export const youtubeSchema = streamCommonSchema.extend({
     streamPrivacy: z.union([z.literal('public'), z.literal('unlisted'), z.literal('private')]),
     latency: z.union([z.literal('normal'), z.literal('low'), z.literal('ultra_low')]),
     afterEndStatus: z.union([z.literal('no_change'), z.literal('public'), z.literal('unlisted'), z.literal('private')]),
-    cameraLedStatus: z.boolean(),
     dvr: z.boolean(),
     saveToSdCard: z.boolean(),
     statusCameraLed: z.boolean(),
